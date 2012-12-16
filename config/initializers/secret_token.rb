3c65e16e2824fa7e0c7127c98e168ea91645ac5f
Eric Kurzenberger <eric@Office-Mini.local>
2012-12-16 12:32:20 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sayhello::Application.config.secret_token = '6392d598e3d5639c0dbcab354974e0e323f79ab034e98e7db9d7829d562d83db45dd29a323f78a863969231205f65aab26f571836167e349acfd93f11a942047'
